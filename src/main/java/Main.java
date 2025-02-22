f7fc37ba16d4a1b49cd20f99b0f325f5ca3ac102
raz0229 <raja.zafar229@gmail.com>
2025-02-22 10:59:58 +0500

    LexScanner added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import java.nio.file.Path;
 public class Main {
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.err.println("Logs from your program will appear here!");
+    //System.err.println("Logs from your program will appear here!");
 
     if (args.length < 2) {
       System.err.println("Usage: ./your_program.sh tokenize <filename>");
@@ -31,9 +31,16 @@ public class Main {
     // Uncomment this block to pass the first stage
     
     if (fileContents.length() > 0) {
-      throw new RuntimeException("Scanner not implemented");
-    } else {
+      // throw new RuntimeException("Scanner not implemented");
+      for (Character ch : fileContents.toCharArray()) {
+        if (ch == '(') {
+          System.out.println("LEFT_PAREN ( null");
+        } else if (ch == ')') {
+          System.out.println("RIGHT_PAREN ) null");
+        }
+      }
+
       System.out.println("EOF  null"); // Placeholder, remove this line when implementing the scanner
-    }
+    } 
   }
 }
